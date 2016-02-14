7947a0c7d64873a32d2da495ff212f0e10efc315
payamka <payam.karimyan@gmail.com>
2016-02-14 13:41:23 +0330

    Centered option for score history month num

--- resources/views/translator/score-history.blade.php
+++ resources/views/translator/score-history.blade.php
@@ -5,18 +5,12 @@
 		<div class="row">
 			<div class="col-sm-12">
 				<h3 class="text-center">{{ trans('common.stats') }}
-					<div class="col-sm-5">
-						<form method="GET">
-							<div class="col-sm-4">
-								<input type="number" class="form-control" name="number" id="number" value="{{ $month_num }}">
-							</div>
-							<div class="col-sm-1">
-								<button type="submit" class="btn btn-default">{{ trans('common.scorehistory_number_option_key') }}</button>
-							</div>
-						</form>
-					</div>
+					
 				</h3>
-				
+				<form method="GET" class="form-inline">
+					<input type="number" class="form-control" name="number" id="number" value="{{ $month_num }}">
+					<button type="submit" class="btn btn-default">{{ trans('common.scorehistory_number_option_key') }}</button>
+				</form>
 				<div class="row">
 					@foreach($score_history as $month=>$sh)
 					<div class="col-sm-12 col-md-3">
