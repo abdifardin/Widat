7cb89cf108d19071d3ae592162f60d445418068d
payamka <payam.karimyan@gmail.com>
2016-02-15 11:37:01 +0330

    Show only one month score in inspector score history

--- app/Http/Controllers/InspectorController.php
+++ app/Http/Controllers/InspectorController.php
@@ -222,13 +222,26 @@ class InspectorController extends Controller
 				$start_date = date('Y-m-1', strtotime("-" . ($history_count - $i) . " months"));
 			}
 			$end_date = date('Y-m-1', strtotime("-" . ($history_count - $i - 1) . " months"));
+			$prev_month = date('Y-m-1', strtotime("-" . ($history_count - $i + 1) . " months"));
 			$sh = ScoreHistory::where('user_id', $user_id)
 				->whereBetween('created_at', [ $start_date, $end_date ])
 				->orderBy('created_at', 'DESC')
 				->first();
-			$score_history[date("F Y", strtotime('-' . ( $history_count - $i ) . ' month'))] = isset($sh->score) ?
-				$sh->score : 0;
 			
+			$shp = ScoreHistory::where('user_id', $user_id)
+				->whereBetween('created_at', [ $prev_month, $start_date ])
+				->orderBy('created_at', 'DESC')
+				->first();
+			
+			if(!isset($shp->score)){
+				$shpc = 0;
+			}else{
+				$shpc = $shp->score;
+			}
+			
+			$score_history[date("F Y", strtotime('-' . ( $history_count - $i ) . ' month'))] = isset($sh->score) ?
+				($sh->score) - $shpc : 0;
+							
 			if(strtotime($start_date) <= strtotime($created_at)){
 				break;
 			}
