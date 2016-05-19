0b4e1875ca92217a5ce39e106ac7d188ba5c0a2b
payamka <payam.karimyan@gmail.com>
2016-05-19 17:27:30 +0430

    Fix autosave problem

--- public/js/autosave.js
+++ public/js/autosave.js
@@ -1,3 +1,12 @@
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
 if($("#ku_trans_abstract").length){
 	window.setInterval(event, 5000);
 	var previous_val = $("#ku_trans_abstract").val();
@@ -8,12 +17,11 @@ if($("#ku_trans_abstract").length){
 	//var autosave_status = false;
 	var retrieve_status = false;
 	var first_time = true;
-	var autosave_lock = false;
 	var have_draft = $("#have_draft").val();
-
+	
 	$('#retrieve_drafts_label').on('click', function (e) {
 		e.preventDefault();
-		autosave_lock = true;
+		first_time = false;
 		$(this).hide();
 		var retrieve = 1;
 		var _token = $('input[name=_token]').val();
@@ -22,8 +30,8 @@ if($("#ku_trans_abstract").length){
 				$('textarea#ku_trans_abstract').summernote('code', result.abstract);
 				$('#ku_trans_title').val(result.topic);
 				updateTranslationScore();
+				sleep(5000);
 			}
-			autosave_lock = false;
 		});
 	});
 		
@@ -31,8 +39,8 @@ if($("#ku_trans_abstract").length){
 		var current_val = $("#ku_trans_abstract").val();
 		var current_title = $("#ku_trans_title").val();
 		
-		if (!autosave_lock && ((current_val.localeCompare(previous_val) != 0 && current_val.localeCompare('<p><br></p>') != 0 && document.getElementById("ku_trans_abstract").value != '')
-		|| (current_title.localeCompare(previous_title) != 0 && document.getElementById("ku_trans_title").value != ''))) {
+		if ((current_val.localeCompare(previous_val) != 0 && current_val.localeCompare('<p><br></p>') != 0 && document.getElementById("ku_trans_abstract").value != '')
+		|| (current_title.localeCompare(previous_title) != 0 && document.getElementById("ku_trans_title").value != '')) {
 			
 			if(first_time){
 				if(have_draft){
@@ -40,7 +48,6 @@ if($("#ku_trans_abstract").length){
 				}
 				
 				if(retrieve_status){
-					autosave_lock = true;
 					var retrieve = 1;
 					var _token = $('input[name=_token]').val();
 					$.post(window.location.href, {retrieve: retrieve, _token: _token}, function(result, status){
@@ -48,26 +55,32 @@ if($("#ku_trans_abstract").length){
 							$('textarea#ku_trans_abstract').summernote('code', result.abstract);
 							$('#ku_trans_title').val(result.topic);
 							updateTranslationScore();
+							sleep(5000);
 						}
-						autosave_lock = false;
 					});
+				}else{
+					run_autosave();
 				}
 				first_time = false;
+			}else{
+				run_autosave();
 			}
-			
-			$("#save_changes_label").show();
-			$("#retrieve_drafts_label").html('<a href="">Draft saved 5 seconds ago, click here to retrieve and click "Save changes".</a>');
-			$("#retrieve_drafts_label").hide();
-			var ku_trans_topic = $("#ku_trans_title").val();
-			var ku_trans_abstract = $("#ku_trans_abstract").val();
-			var autosave = 1;
-			var _token = $('input[name=_token]').val();
-			$.post(window.location.href, {ku_trans_topic: ku_trans_topic, ku_trans_abstract: ku_trans_abstract, autosave: autosave, _token: _token}, function(result, status){
-				$("#save_changes_label").hide();
-				$("#retrieve_drafts_label").show();
-			});
 		}
 		previous_val = current_val;
 		previous_title = current_title;
+	}
+		
+	function run_autosave() {
+		$("#save_changes_label").show();
+		$("#retrieve_drafts_label").html('<a href="">Draft saved 5 seconds ago, click here to retrieve and click "Save changes".</a>');
+		$("#retrieve_drafts_label").hide();
+		var ku_trans_topic = $("#ku_trans_title").val();
+		var ku_trans_abstract = $("#ku_trans_abstract").val();
+		var autosave = 1;
+		var _token = $('input[name=_token]').val();
+		$.post(window.location.href, {ku_trans_topic: ku_trans_topic, ku_trans_abstract: ku_trans_abstract, autosave: autosave, _token: _token}, function(result, status){
+			$("#save_changes_label").hide();
+			$("#retrieve_drafts_label").show();
+		});
 	}
 }
